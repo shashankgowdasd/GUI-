f1a91321b58093ffd7d0523abbb8894848cf06a1
Shashank S D <shashank.sd@inubesolutions.net>
2020-10-21 23:38:08 +0530

    its my first commit using GUI github

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var name = new Student();
+           name.add(23.6);
+           name.add(50.45);
+           name.add(67.8);
+           name.add(67.5);
+           
+           var result = name.getstatistics();
+            Console.WriteLine($" avaerage grade:{result.average}");
+            Console.WriteLine($" avaerage grade:{result.high}");
+            Console.WriteLine($" avaerage grade:{result.low}");
+        }
+    }
+}
